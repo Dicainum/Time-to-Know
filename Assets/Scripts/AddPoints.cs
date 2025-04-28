7132620204fa04b292ed1eb2468bcf9b20afd2ab
Bulat Igebaev <pingvin13lol@gmail.com>
2025-04-28 21:51:44 +0500

    synchronized points

--- Assets/Scripts/AddPoints.cs
+++ Assets/Scripts/AddPoints.cs
@@ -47,7 +47,41 @@ public class AddPoints : MonoBehaviourPun
         else
         {
             points = _question.points;
-            targetPlayer.points += points; 
+            targetPlayer.points += points;
+            SyncAllPlayersPoints();
+        }
+        
+    }
+
+    private void SyncAllPlayersPoints()
+    {
+        var allPoints = new int[_players.Length];
+
+        for (int i = 0; i < _players.Length; i++)
+        {
+            allPoints[i] = _players[i].points;
+        }
+
+        photonView.RPC("SynchPoints", RpcTarget.AllBuffered, allPoints);
+    }
+
+    [PunRPC]
+    private void SynchPoints(int[] allPoints)
+    {
+        if (_players == null || _players.Length == 0)
+        {
+            var playersGO = GameObject.FindGameObjectsWithTag("Player");
+            _players = new NetworkPlayer[playersGO.Length];
+            for (int i = 0; i < playersGO.Length; i++)
+            {
+                _players[i] = playersGO[i].GetComponent<NetworkPlayer>();
+            }
+        }
+
+        for (int i = 0; i < _players.Length; i++)
+        {
+            _players[i].points = allPoints[i];
+            _players[i].UpdatePoints();
         }
     }
     
